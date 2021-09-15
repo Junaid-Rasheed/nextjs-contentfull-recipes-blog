27371390aece6e9951be5e77c0796ba16a4601dd
Junaid-Rasheed <junaidrasheed300@gmail.com>
2021-09-15 18:15:34 +0500

    first commit

---
+++ components/RecipeCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+function RecipeCard({ recipe }) {
+  const { cookingTime, slug, title, thumbnail } = recipe.fields;
+  return (
+    <div>
+      <Image
+        className="card"
+        src={"https:" + thumbnail.fields.file.url}
+        // height={thumbnail.fields.file.details.image.height}
+        // width={thumbnail.fields.file.details.image.width}
+        width={350}
+        height={270}
+      />
+
+      <div className="content">
+        <h3>{title}</h3>
+        <p>Takes approx {cookingTime} mins to cook</p>
+
+        <Link href={'/recipes/' + slug}>
+          <a>
+            <h5>Cook This</h5>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default RecipeCard;
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,71 @@
-export default function RecipeDetails() {
+import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
+
+// GetStaticPaths
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+// GetStaticProps
+
+export const getStaticProps = async ({ params }) => {
+  const res = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+  return {
+    props: {
+      recipe: res.items[0],
+    },
+  };
+};
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
+  const { featuredImage, cookingTime, title, ingredient, method } =
+    recipe.fields;
   return (
     <div>
-      Recipe Details
+      <div className='banner'>
+        <Image
+          src={"https:" + featuredImage.fields.file.url}
+          // height={featuredImage.fields.file.details.image.height}
+          // width={featuredImage.fields.file.details.image.width}
+          width={1100}
+          height={370}
+        />
+        <h1 >{title}</h1>
+      </div>
+
+      <div className="info">
+        <p> It take approx {cookingTime} mins</p>
+        <h3>Ingrediants : </h3>
+        {ingredient.map((obj) => (
+          <span key={obj}>{obj}</span>
+        ))}
+      </div>
+      <div className="method">
+        <h3>Method : </h3>
+        {documentToReactComponents(method)}
+      </div>
     </div>
-  )
+  );
 }
